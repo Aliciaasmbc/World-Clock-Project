863cda9e84692e29cf3e0bf3ff8801a1f762a0f3
Aliciaasmbc <alicia.apreala@gmail.com>
2023-08-15 21:59:32 +0100

    injected real data into the page

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  //New York
+  let newyorkElement = document.querySelector("#new-york");
+  let newyorkDateElement = newyorkElement.querySelector(".date");
+  let newyorkTimeElement = newyorkElement.querySelector(".time");
+  let newyorkTime = moment().tz("America/New_York");
 
+  newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
+  newyorkTimeElement.innerHTML = newyorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
